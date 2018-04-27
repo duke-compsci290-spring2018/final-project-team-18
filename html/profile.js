edfdf8569f5b73d65b9f0be007c2eaf5a1e5ca16
Judyth <jke10@duke.edu>
2018-04-27 00:02:41 -0400

    adding login modal'

--- html/profile.js
+++ html/profile.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         title: "Meet Others",
-        profiles: [{
+        profiles: [{ //list of profiles, might add to separate JSON file later
             'name': "Judyth Estrada",
             'major': 'Computer Science',
             'class': '2020',
@@ -20,5 +20,24 @@ var app = new Vue({
             'class': '2019',
             'description': 'hello'
         }]
+    },
+    methods: {
+        open: function(which, e) { //function to open the modal for login
+            e.preventDefault();
+            $('#login-modal').addClass('active'); //add active class to show that modal is open or not
+        }
+    }
+})
+
+var modal = new Vue ({
+    el: '#login-modal',
+    methods: {
+        close: function(e) {
+            e.preventDefault();
+            if (e.target === this.$el) {
+                $('#login-modal').removeClass('active'); 
+            }
+            
+        }
     }
 })
