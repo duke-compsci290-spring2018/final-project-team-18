d2d44773692553854f0511615b3ca85cda8afe28
Judyth <jke10@duke.edu>
2018-04-26 23:27:02 -0400

    added profiles

---
+++ html/profile.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: "Meet Others",
+        profiles: [{
+            'name': "Judyth Estrada",
+            'major': 'Computer Science',
+            'class': '2020',
+            'description': 'hello'
+        },
+        {
+            'name': 'Dania Estrada',
+            'major': 'Computer Science',
+            'class': '2019',
+            'description': 'hello'
+        },
+        {
+            'name': 'Alejandra Aguilar',
+            'major': 'idk',
+            'class': '2019',
+            'description': 'hello'
+        }]
+    }
+})
