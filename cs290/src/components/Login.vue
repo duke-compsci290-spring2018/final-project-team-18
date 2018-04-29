ede22625866f87dcbc68b27db3e055d2dbee2484
Dania <dae19@duke.edu>
2018-04-29 02:25:56 -0400

    new log in

--- cs290/src/components/Login.vue
+++ cs290/src/components/Login.vue
@@ -1,32 +1,40 @@
 <template lang="html">
+  <div id="bg">
+    <div id="bgq"></div>
   <div id="firebaseui-auth-container"></div>
+</div>
 </template>
 
 <script>
 import firebase from 'firebase';
-import firebaseui from 'firebaseui'
+import firebaseUI from 'firebaseui'
 import {config} from '../db';
 export default {
   name: 'Login',
   mounted() {
     var uiConfig = {
+    // require password each time
+                credentialHelper: firebaseUI.auth.CredentialHelper.NONE,
+                signInFlow: 'popup',
       signInSuccessUrl: '/home',
       signInOptions:[{
                     provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                    requireDisplayName: true
-                }]
-                   
-      };
+                    requireDisplayName: true,
+                }],
+   };
     var ui = new firebaseui.auth.AuthUI(firebase.auth());
     ui.start('#firebaseui-auth-container', uiConfig);
+
+                 
     },
 }
+
 </script>
 
-<style lang="css">
+<style >
 
 #firebaseui-auth-container{
-text-align: center;
+    text-align: center;
     margin: 0 40% ;
     margin-top: -100px;
     position: fixed;
@@ -36,4 +44,51 @@ text-align: center;
     box-shadow: 0px 0px 25px 3px rgba(255,255,255,0.6);
     color: white;
 }
+
+button{
+    width: 100px;
+
+    color:black;
+}
+
+html{
+    background-image: url(../assets/images/flower.gif);
+    background-repeat: no-repeat;
+    height: 100%;
+    background-size: cover;
+}
+#bgq{
+    background-color: rgba(0,0,0,0.6);
+    min-height: 100%;
+    min-width: 100%;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+}
+input{
+width: 200px;
+}
+#home{
+    text-align: center;
+    margin: 0 40% ;
+    margin-top: -100px;
+    position: fixed;
+    top: 50%;
+    padding: 15px 60px;
+    border-radius: 50px;
+    box-shadow: 0px 0px 25px 3px rgba(255,255,255,0.6);
+    color: white;
+} 
+
+a{
+    text-decoration: none;
+}
+h1{
+    font-family: 'Nanum Gothic', sans-serif;
+    letter-spacing: 3px;
+    font-size: 15px;
+}
+
 </style>
